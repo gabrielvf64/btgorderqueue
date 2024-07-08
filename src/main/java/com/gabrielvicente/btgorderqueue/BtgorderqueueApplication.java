f832e6b73758793d45e71f8e3bc81c8feb999b0d
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-08 16:36:04 -0300

    initial commit

---
+++ src/main/java/com/gabrielvicente/btgorderqueue/BtgorderqueueApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.btgorderqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgorderqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgorderqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielvicente/btgorderqueue/BtgorderqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.btgorderqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgorderqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

358046119d99d7b857400548f53ba0faaa5e33bc
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-17 14:54:24 -0300

    test: check parameters passed

--- src/test/java/com/gabrielvicente/btgorderqueue/controller/OrderControllerTest.java
+++ src/test/java/com/gabrielvicente/btgorderqueue/controller/OrderControllerTest.java
@@ -7,9 +7,12 @@ import com.gabrielvicente.btgorderqueue.service.OrderService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
@@ -29,6 +32,12 @@ class OrderControllerTest {
     @InjectMocks
     OrderController orderController;
 
+    @Captor
+    ArgumentCaptor<Long> customerIdCaptor;
+
+    @Captor
+    ArgumentCaptor<PageRequest> pageRequestCaptor;
+
     @Nested
     class ListOrders {
 
@@ -47,6 +56,27 @@ class OrderControllerTest {
             ResponseEntity<ApiResponse<OrderResponse>> response = orderController.listOrders(customerId, page, pageSize);
 
             assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        }
+
+        @Test
+        void shouldPassCorrectParametersToService() {
+            var customerId = 1L;
+            var page = 0;
+            var pageSize = 10;
+
+            doReturn(OrderResponseFactory.buildWithDefaultValues())
+                    .when(orderService).findAllByCustomerId(customerIdCaptor.capture(), pageRequestCaptor.capture());
+
+            doReturn(BigDecimal.valueOf(20.50))
+                    .when(orderService).findTotalSpentByCustomerId(customerIdCaptor.capture());
+
+            ResponseEntity<ApiResponse<OrderResponse>> response = orderController.listOrders(customerId, page, pageSize);
+
+            assertEquals(2, customerIdCaptor.getAllValues().size());
+            assertEquals(customerId, customerIdCaptor.getAllValues().get(0));
+            assertEquals(customerId, customerIdCaptor.getAllValues().get(1));
+            assertEquals(page, pageRequestCaptor.getValue().getPageNumber());
+            assertEquals(pageSize, pageRequestCaptor.getValue().getPageSize());
         }
     }
 }
